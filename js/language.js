5ba3bef36cc04f1dc1cc9e194ada295dea7a4932
JBLGithub <jordi.laird@gmail.com>
2022-06-17 23:11:39 +0200

    first dutch

--- js/language.js
+++ js/language.js
@@ -26,6 +26,26 @@ function languageChange(language)
 	}
 	else if(language == "NL")
 	{
+		document.getElementById("welcometitle").innerHTML = "Welkom op onze bruiloft website!";
+		document.getElementById("welcometext").innerHTML = "On this page, you will find all the information you need involving the wedding.";
+
+		document.getElementById("datelocationtitle").innerHTML = "Dates and Location";
+		document.getElementById("datelocationtext").innerHTML = "Please familiarise yourself with where you need to be and when.";
+		document.getElementById("datelocationtiming").innerHTML = "Please arrive at 30 Hakkesstraat, 5916 PX Venlo on the 2nd of July for 2.00PM.";
+		
+		document.getElementById("busestitle").innerHTML = "Buses";
+		document.getElementById("busestext").innerHTML = "Buses to and from the venue will be provided.";
+		document.getElementById("busestext1").innerHTML = "Leaving from Roompot Vakanties Resort Arcen at 1:30pm. The return trip leaves at 2:00am.";
+		document.getElementById("busestext2").innerHTML = "Leaving from Kruisweg 10, 5944 EN Arcen at 1:40pm. The return trip leaves at 2:00am.";
+
+		document.getElementById("dresscodetitle").innerHTML = "Dress Code";
+		document.getElementById("dresscodetext").innerHTML = "The dress code is Colourful and Festive!";
+
+		document.getElementById("contacttitle").innerHTML = "Contact Us";
+		document.getElementById("contacttext").innerHTML = "Please don't hesitate to get in touch. We are pleased to answer any questions.";
+
+		document.getElementById("donationtitle").innerHTML = "Wedding Gift";
+		document.getElementById("donationtext").innerHTML = "We are delighted that you have made it to our wedding. If you do wish to make a contribution towards our honeymoon please use the link below:";
 		console.log("Dutch");
 	}
 	else
