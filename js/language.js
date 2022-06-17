c6ab3559ca0562c0822997a59f22f2eed3efe38f
JBLGithub <jordi.laird@gmail.com>
2022-06-17 23:26:16 +0200

    last dutch

--- js/language.js
+++ js/language.js
@@ -35,17 +35,17 @@ function languageChange(language)
 		
 		document.getElementById("busestitle").innerHTML = "De Bus";
 		document.getElementById("busestext").innerHTML = "De bus van en naar de trouwlocatie wordt voor u geregeld.";
-		document.getElementById("busestext1").innerHTML = "Leaving from Roompot Vakanties Resort Arcen at 1:30pm. The return trip leaves at 2:00am.";
-		document.getElementById("busestext2").innerHTML = "Leaving from Kruisweg 10, 5944 EN Arcen at 1:40pm. The return trip leaves at 2:00am.";
+		document.getElementById("busestext1").innerHTML = "De bus vertrekt om 13:30 vanuit Roompot Resort Arcen naar de trouwlocatie, en vertekt om 02:00 uur vanuit de trouwlocatie naar Resort Arcen.";
+		document.getElementById("busestext2").innerHTML = "De bus vertrekt om 13:40 vanuit Kruisweg 10 Arcen naar de trouwlocatie, en vertekt om 02:00 uur vanuit de trouwlocatie naar de Kruisweg 10 te Arcen.";
 
 		document.getElementById("dresscodetitle").innerHTML = "Dress Code";
-		document.getElementById("dresscodetext").innerHTML = "The dress code is Colourful and Festive!";
+		document.getElementById("dresscodetext").innerHTML = "De dress code is kleurrijk en feestelijk!";
 
-		document.getElementById("contacttitle").innerHTML = "Contact Us";
-		document.getElementById("contacttext").innerHTML = "Please don't hesitate to get in touch. We are pleased to answer any questions.";
+		document.getElementById("contacttitle").innerHTML = "Neem Contact met ons op";
+		document.getElementById("contacttext").innerHTML = "Aarzel niet om contact op te nemen, wij beantwoorden graag al uw vragen.";
 
-		document.getElementById("donationtitle").innerHTML = "Wedding Gift";
-		document.getElementById("donationtext").innerHTML = "We are delighted that you have made it to our wedding. If you do wish to make a contribution towards our honeymoon please use the link below:";
+		document.getElementById("donationtitle").innerHTML = "Huwelijks Cadeau";
+		document.getElementById("donationtext").innerHTML = "Wij verheugen ons erop om jullie te zien op onze bruiloft, mocht je een bijdrage willen geven voor onze huwelijksreis, gebruik dan deze link hieronder:";
 		console.log("Dutch");
 	}
 	else
